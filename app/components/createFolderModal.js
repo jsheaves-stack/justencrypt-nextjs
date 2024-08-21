f54af42641bf26227af9a90ca22bf1c3121b03c4
Justin Sheaves <jsheaves@protonmail.com>
2024-08-20 22:46:42 -0400

    Condensed the create folder modal a little bit

--- app/components/createFolderModal.js
+++ app/components/createFolderModal.js
@@ -17,14 +17,14 @@ export default function CreateFolderModal(props) {
 
   return (
     <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-overlay text-text">
-      <div className="relative flex w-[300px] flex-col items-center justify-center rounded-base border-2 border-border bg-bg p-10 pt-12 font-base shadow-base">
+      <div className="relative flex w-[300px] flex-col items-center justify-center rounded-base border-2 border-border bg-bg p-6 pt-12 font-base shadow-base">
         <button
           className="text-align-center w-18 absolute right-4 top-4 ml-2 flex cursor-pointer rounded-base border-2 border-black bg-main px-4 py-2 text-sm font-base shadow-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none"
           onClick={props.closeModal}
         >
           X
         </button>
-        <div className="mt-2 grid grid-rows-[2em,1fr]">
+        <div className="grid grid-rows-[2em,1fr] gap-2">
           <label>Folder Name</label>
           <input
             type="text"
@@ -36,7 +36,7 @@ export default function CreateFolderModal(props) {
           />
         </div>
         <button
-          className="text-align-center w-18 ml-2 mt-8 flex cursor-pointer rounded-base border-2 border-black bg-main px-4 py-2 text-sm font-base shadow-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none"
+          className="text-align-center w-18 ml-2 mt-4 flex cursor-pointer rounded-base border-2 border-black bg-main px-4 py-2 text-sm font-base shadow-base transition-all hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none"
           onClick={() => createFolder()}
         >
           Ok
