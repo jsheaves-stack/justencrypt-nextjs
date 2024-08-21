bcfb03514a40a81bf9c395a560862fd0c0dd5093
Justin Sheaves <jsheaves@protonmail.com>
2024-08-21 19:48:39 -0400

    Replaced downloading a file with an anchor tag as it'll stream the whole file to disk instead of loading the whole thing into memory which was a problem for large files.

--- app/components/file.js
+++ app/components/file.js
@@ -5,26 +5,6 @@ import { sanitizePath } from '../utils/util';
 import { Controlled as ControlledZoom } from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
-function downloadFile(url, filename) {
-  fetch(url, { credentials: 'include' })
-    .then((response) => response.blob())
-    .then((blob) => {
-      const blobUrl = window.URL.createObjectURL(blob);
-      const anchor = document.createElement('a');
-
-      anchor.href = blobUrl;
-      anchor.download = filename;
-
-      document.body.appendChild(anchor);
-
-      anchor.click();
-
-      document.body.removeChild(anchor);
-      window.URL.revokeObjectURL(blobUrl);
-    })
-    .catch((error) => console.error('Error downloading file:', error));
-}
-
 export default function File(props) {
   const api_url = process.env.NEXT_PUBLIC_JUSTENCRYPT_API_URL;
   const supported_images = ['APNG', 'AVIF', 'GIF', 'JPG', 'JPEG', 'PNG', 'SVG', 'WebP'];
@@ -36,13 +16,6 @@ export default function File(props) {
 
   const image_url = `${api_url}${sanitizePath(`${'/file'}/${file_path}`)}`;
   const thumbnail_image_url = `${api_url}${sanitizePath(`${'/thumbnail'}/${file_path}`)}`;
-
-  const handleDownload = (e) => {
-    const fileUrl = `${api_url}${sanitizePath(`file/${file_path}`)}`;
-    const filename = props.file.file_name;
-
-    downloadFile(fileUrl, filename);
-  };
 
   return (
     <div className="grid h-full w-full cursor-pointer grid-rows-1 overflow-hidden rounded-base border-2 border-black bg-main font-base shadow-base">
@@ -58,11 +31,10 @@ export default function File(props) {
         <div className="grid h-full w-full items-center justify-items-center text-4xl">{file_extension}</div>
       )}
 
-      <div
-        className="h-10 w-full overflow-hidden truncate text-ellipsis text-wrap border-t-2 border-black bg-mainAccent p-2 text-sm"
-        onClick={handleDownload}
-      >
-        <span className="overflow-hidden truncate text-ellipsis text-wrap text-text">{props.file.file_name}</span>
+      <div className="h-10 w-full overflow-hidden truncate text-ellipsis text-wrap border-t-2 border-black bg-mainAccent p-2 text-sm">
+        <a href={`${api_url}${sanitizePath(`file/${file_path}`)}`}>
+          <span className="overflow-hidden truncate text-ellipsis text-wrap text-text">{props.file.file_name}</span>
+        </a>
       </div>
     </div>
   );
