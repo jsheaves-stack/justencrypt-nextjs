b18b7d0e1f9eb8b2e7c040efde39166ad543587f
Justin Sheaves <jsheaves@protonmail.com>
2024-09-21 20:56:26 -0400

    Added title

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import './globals.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>JustEncrypt</title>
       <body className="h-full w-full">{children}</body>
     </html>
   );
